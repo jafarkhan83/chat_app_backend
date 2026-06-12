4b339c6ec7487b56fb3e006fcbc6000b4906faa2
jafarkhan83 <jafarkhan.master@gmail.com>
2026-06-12 11:36:08 +0500

    Implementing authentication via tokenization.

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/users', [ChatController::class, 'getUsers']);
     Route::get('/messages/{user_id}', [ChatController::class, 'getMessages']);
     Route::post('/messages', [ChatController::class, 'sendMessage']);
